96b31577d2a2432434c3fa2c3cc7cbb79a20fb10
RanaUniverse <RanaUniverse321@gmail.com>
2025-10-12 05:42:40 +0530

    ready a website simple with my custom css also with layout and index page only and the main.py file will run now i have updated the README file to keep record my informations of what i will do

---
+++ README.md
@@ -0,0 +1,8 @@
+This will be the making of a website of shopping e commerce from scratch in python.
+
+I think to make it a extensable website.
+
+Some Variable Globally:
+
+* BUSINESS_NAME
+
--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from shopping-website-python!")
+import os
+
+
+from dotenv import load_dotenv
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+load_dotenv()
+
+BUSINESS_NAME = os.environ.get("BUSINESS_NAME")
+
+
+@app.route("/")
+def home_page():
+    return render_template(
+        "index.html",
+    )
+
+
+@app.context_processor
+def inject_global_variable():
+    return dict(
+        BUSINESS_NAME=BUSINESS_NAME,
+    )
 
 
 if __name__ == "__main__":
-    main()
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        debug=True,
+    )
